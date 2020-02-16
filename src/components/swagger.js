1d9f3a536f1d6cbbfdebe47e3747d19aafbac38b
Chijioke <cugwuanyi@odvisor.com>
2020-02-16 17:59:26 +0000

    Fixed variable declaration.

--- src/components/swagger.js
+++ src/components/swagger.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
  
-export default Swagger = () => <SwaggerUI url="https://github.com/xplorer1/bucketlist/blob/master/swagger.json" />
+const Swagger = () => <SwaggerUI url="https://github.com/xplorer1/bucketlist/blob/master/swagger.json" />
+
+export default Swagger;
