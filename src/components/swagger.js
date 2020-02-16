eadfbe2b6d33c8969e89fcad5ddaf522d4b8f0e6
Chijioke <cugwuanyi@odvisor.com>
2020-02-16 17:32:47 +0000

    Testing swagger.

---
+++ src/components/swagger.js
@@ -0,0 +1,4 @@
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+ 
+export default Swagger = () => <SwaggerUI url="https://github.com/xplorer1/bucketlist/blob/master/swagger.json" />
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import SignUp from "./components/signup";
 import NotFound from "./components/notfound";
 import BucketList from "./components/bucketlist";
 import Item from "./components/item";
+import Swagger from "./components/swagger";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
 	    		<Route exact path='/signup' component={SignUp} />
 	    		<Route exact path='/bucketlist' component={BucketList} />
 	    		<Route exact path='/item' component={Item} />
+	    		<Route exact path='/api/vi/api-docs' component={Swagger} />
 	    		<Route path='' component={NotFound} />
 			</Switch>
         </Router>
